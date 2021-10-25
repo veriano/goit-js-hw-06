0355a05d16253f4008c322ef07517c5785804c9c
veriano <86019033+veriano@users.noreply.github.com>
2021-10-25 20:42:09 +0300

    update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -4,7 +4,7 @@ form.setAttribute('autocomplete', 'current-password');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-     const {elements: { email, password }} = event.currentTarget;    
+     const {elements: { email, password },} = event.currentTarget;    
 
     if (email.value === "" || password.value === "") {
         alert("Please fill in all the fields!");
