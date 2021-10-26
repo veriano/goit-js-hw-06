f86874ae3173b133ad9f58e6aa115cac6adcfa70
veriano <86019033+veriano@users.noreply.github.com>
2021-10-26 11:17:16 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,25 +1,13 @@
-const list = document.querySelector('#categories')
-const totalItem = document.querySelectorAll('.item').length;
+const list = document.querySelector('#categories');
+const allCategories = list.querySelectorAll('h2');
+console.log(`Number of categories: ${allCategories.length}`);
 
-console.log(`Number of categories: ${totalItem}`);
+const allLi = list.querySelectorAll('.item');
+allLi.forEach(el => {
+    const getTitle = el.querySelector('h2');
+    console.log(`Category: ${getTitle.textContent}`)
 
-const allTitles = document.querySelectorAll('h2');
-console.log(`Category: ${allTitles[0].textContent}`);
-
-const getItem1 = document.querySelector('.item');
-const getElements1 = getItem1.querySelectorAll('li').length;
-
-console.log(`Elements: ${getElements1}`);
-
-const getItems = document.querySelectorAll('.item');
-const getItem2 = getItems[1];
-const getElements2 = getItem2.querySelectorAll('li').length;
-
-console.log(`Category: ${allTitles[1].textContent}`);
-console.log(`Elements: ${getElements2}`)
-
-const getItems3 = getItems[2];
-const getElements3 = getItems3.querySelectorAll('li').length;
-
-console.log(`Category: ${allTitles[2].textContent}`);
-console.log(`Elements: ${getElements3}`);
+    const getItemList = el.querySelector('ul');
+    const getLi = getItemList.querySelectorAll('li');
+    console.log(`Elements: ${getLi.length}`)
+});
