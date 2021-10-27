1aa1384ddea204651655bb4596a5fa5f8b813430
veriano <86019033+veriano@users.noreply.github.com>
2021-10-27 23:14:11 +0300

    Update task -08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -4,12 +4,11 @@ form.setAttribute('autocomplete', 'current-password');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-     const {elements: { email, password },} = event.currentTarget;    
 
     if (email.value === "" || password.value === "") {
         alert("Please fill in all the fields!");
     }
 
-    console.log(event.currentTarget);
-    event.currentTarget.reset();
+    console.log({ email: email.value, password: password.value });
+    { email: email.value, password: password.value}.reset();
 })
